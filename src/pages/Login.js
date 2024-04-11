c5360842ca1650d68c276934c9a2c7d7e525cc82
hamza walker <hamzawalker.vienna@gmail.com>
2024-04-11 13:55:09 +0200

    styled the log in page, created a form for registration along with the proper functionality to connect to firebase and update the user name as well as using dipatch to push the login information to the redux user object

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import "./Login.css";
+import { auth } from "../db/firebase";
+import { login } from "../features/userSlice";
+import { updateProfile, createUserWithEmailAndPassword } from "firebase/auth";
 
 export const Login = () => {
-	return (
-		<div className="login">
-			<h1>Sign in</h1>
-		</div>
-	);
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [profilePic, setProfilePic] = useState("");
+  const dispatch = useDispatch();
 
+  const loginToApp = (e) => {
+    e.preventDefault();
+  };
+
+  const register = () => {
+    if (!name) {
+      return alert("Please enter a full name");
+    }
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // The user has been created successfully
+        const user = userCredential.user;
+        updateProfile(user, {
+          displayName: name,
+          photoURL: profilePic,
+        })
+          .then(() => {
+            dispatch(
+              login({
+                email: user.email,
+                uid: user.uid,
+                displayName: name,
+                photoUrl: profilePic,
+              }),
+            );
+          })
+          .catch((error) => alert(error.message));
+      })
+      .catch((error) => alert(error.message));
+  };
+  return (
+    <div className="login">
+      <img
+        src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Logo.svg.original.svg"
+        alt=""
+      />
+      <form>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="username (required)"
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="password"
+        />
+        <input
+          value={profilePic}
+          onChange={(e) => setProfilePic(e.target.value)}
+          type="text"
+          placeholder=" Profile URL (optional)"
+        />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder=" Email (optional)"
+        />
+        <button type="submit" onClick={loginToApp}>
+          Sign In
+        </button>
+      </form>
+      <p>
+        Not a member?{" "}
+        <span className="login__register" onClick={register}>
+          Register Now
+        </span>
+      </p>
+    </div>
+  );
+};
