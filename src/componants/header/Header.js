ad7e3f46923523f53c55cd08ab479ad0d8b10d1b
hamza walker <hamzawalker.vienna@gmail.com>
2024-04-12 19:04:39 +0200

    successfully deployed to firebase, project was fun and might continue working on it in the future but for now my objectives were met

--- src/componants/header/Header.js
+++ src/componants/header/Header.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import HomeIcon from "@mui/icons-material/Home";
-import GroupIcon from '@mui/icons-material/Group';
-import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
-import MessageIcon from '@mui/icons-material/Message';
-import NotificationsIcon from '@mui/icons-material/Notifications';
+import GroupIcon from "@mui/icons-material/Group";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import MessageIcon from "@mui/icons-material/Message";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 import { HeaderOptions } from "../headerOptions/HeaderOptions";
 import { useDispatch } from "react-redux";
 import { auth } from "../../db/firebase";
@@ -14,14 +14,13 @@ export const Header = () => {
 	const dispatch = useDispatch();
 	const logoutOfApp = () => {
 		dispatch(logout());
-		auth.signOut(); 
-	}
+		auth.signOut();
+	};
 	return (
 		<div className="header">
-
 			<div className="header__left">
 				<img
-					src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/LinkedIn_2021.svg/400px-LinkedIn_2021.svg.png"
+					src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
 					alt="logo"
 				/>
 				<div className="header__search">
@@ -30,16 +29,12 @@ export const Header = () => {
 				</div>
 			</div>
 			<div className="header__right">
-				<HeaderOptions Icon={HomeIcon}  title="Home" />
+				<HeaderOptions Icon={HomeIcon} title="Home" />
 				<HeaderOptions Icon={GroupIcon} title="Network" />
 				<HeaderOptions Icon={BusinessCenterIcon} title="Jobs" />
 				<HeaderOptions Icon={MessageIcon} title="Messaging" />
 				<HeaderOptions Icon={NotificationsIcon} title="Notifications" />
-				<HeaderOptions
-					avatar = {true}
-					title="me" 
-					onClick={logoutOfApp}
-				/>
+				<HeaderOptions avatar={true} title="me" onClick={logoutOfApp} />
 			</div>
 		</div>
 	);
